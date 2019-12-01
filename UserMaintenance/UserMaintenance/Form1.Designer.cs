4ad523c8c89c5f8abe8c290b3e5ab28a2f1e53d5
potsatamas97 <potsatamas97@gmail.com>
2019-12-01 19:03:09 +0100

    Adatforrások bekötése

--- UserMaintenance/UserMaintenance/Form1.Designer.cs
+++ UserMaintenance/UserMaintenance/Form1.Designer.cs
@@ -28,7 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listUsers = new System.Windows.Forms.ListBox();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.btnAdd = new System.Windows.Forms.Button();
@@ -36,13 +36,13 @@
             this.lblFirstName = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // listBox1
+            // listUsers
             // 
-            this.listBox1.FormattingEnabled = true;
-            this.listBox1.Location = new System.Drawing.Point(13, 30);
-            this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(256, 251);
-            this.listBox1.TabIndex = 0;
+            this.listUsers.FormattingEnabled = true;
+            this.listUsers.Location = new System.Drawing.Point(13, 30);
+            this.listUsers.Name = "listUsers";
+            this.listUsers.Size = new System.Drawing.Size(256, 251);
+            this.listUsers.TabIndex = 0;
             // 
             // textBox1
             // 
@@ -95,7 +95,7 @@
             this.Controls.Add(this.btnAdd);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.listUsers);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -105,7 +105,7 @@
 
         #endregion
 
-        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listUsers;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.Button btnAdd;
--- UserMaintenance/UserMaintenance/Form1.cs
+++ UserMaintenance/UserMaintenance/Form1.cs
@@ -12,12 +12,30 @@ namespace UserMaintenance
 {
     public partial class Form1 : Form
     {
+        BindingList<Entities.User> users = new BindingList<Entities.User>();
+
         public Form1()
         {
             InitializeComponent();
             lblLastName.Text = Resource.LastName; // label1
             lblFirstName.Text = Resource.FirstName; // label2
             btnAdd.Text = Resource.Add; // button1
+            btnAdd.Click += BtnAdd_Click;
+
+            listUsers.DataSource = users;
+            listUsers.ValueMember = "ID";
+            listUsers.DisplayMember = "FullName";
+
+        }
+
+        private void BtnAdd_Click(object sender, EventArgs e)
+        {
+            var u = new Entities.User()
+            {
+                LastName = textBox1.Text,
+                FirstName = textBox2.Text
+            };
+            users.Add(u);
         }
     }
 }
